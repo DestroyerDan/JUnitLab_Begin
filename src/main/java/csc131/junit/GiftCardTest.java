889443961c4f9a40147510cf7eaf5658c595032d
Jared <jconatser@csus.edu>
2021-03-04 10:53:35 -0800

    Finished with lab.

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -41,4 +41,46 @@ public class GiftCardTest
 		card = new GiftCard(issuingStore, balance);
 		assertEquals("getBalance()", "Remaining Balance:  95.00", card.deduct(5));
 	}
+	
+	@Test
+	public void deduct_AmountDue() 
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		issuingStore = 1337;
+		balance = 10.00;
+		card = new GiftCard(issuingStore, balance);
+		assertEquals("getBalance()", "Amount Due:   5.00", card.deduct(15));
+	}
+	
+	@Test
+	public void deduct_InvalidAmount() 
+	{
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		assertEquals("getBalance()", "Invalid Transaction", card.deduct(-5));
+	}
+	
+	@Test
+	public void constructor_IncorrectBalance_Low()
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(1,-100.00);});
+	}
+	
+	@Test
+	public void constructor_IncorrectID_Low()
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(-10,100.00);});
+	}
+	
+	@Test
+	public void constructor_IncorrectID_High()
+	{
+		assertThrows(IllegalArgumentException.class, () -> {new GiftCard(10000,100.00);});
+	}
 }
